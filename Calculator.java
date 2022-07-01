8fd823459ddce4a220b277dabc50448b09a10383
swetha <kakaraparthi.swetha@gmail.com>
2022-07-01 16:34:41 +0530

    commiting the changes after incorporating the review comments

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,8 @@
+/**
+ * this class contains the functions related to arthimetic operations
+ * @since 1.0
+ * @version 1.0
+ */
 public class Calculator{
     public int sum(int a,int b){
         return a + b;
