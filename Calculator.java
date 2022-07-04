de4ed7855fe81ab6177d8fbbea86c461a6ac6a35
swetha <kakaraparthi.swetha@gmail.com>
2022-07-04 13:49:57 +0530

    commiting

--- Calculator.java
+++ Calculator.java
@@ -7,12 +7,7 @@ public class Calculator{
     public int sum(int a,int b){
         return a + b;
     }
-<<<<<<< HEAD
-    public int subtract(int a,int b){
-        return a-b;
-=======
-    public int product(int a, int b){
+    public int product(int a,int b){
         return a * b;
->>>>>>> 635009ae474b5ec41beffba1f7922a17e7dc3aef
     }
 }
